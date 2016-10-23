e1c01f24ba73f30763cd0d5027ec2951e605383a
Ornidon <thib.loiseau@gmail.com>
2016-10-23 15:50:02 +0200

    creating film manager part 1

--- projet/src/main/java/com/mycompany/projet/services/FilmManager.java
+++ projet/src/main/java/com/mycompany/projet/services/FilmManager.java
@@ -2,6 +2,7 @@ package com.mycompany.projet.services;
 
 import com.mycompany.projet.models.Actor;
 import com.mycompany.projet.models.Film;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
@@ -15,5 +16,41 @@ public class FilmManager implements FilmManagerLocal{
     @EJB
     private MySQLCommon QueryExecutor;
     
+    private final String GET_FILM_QUERY = "";
+    private final String GET_FILMS_QUERY = "";
+    private final String GET_ACTORS_QUERY = "";
+    private final String CREATE_FILM_QUERY = "";
+    private final String DELETE_FILM_QUERY = "";
+    private final String UPDATE_FILM_QUERY = "";
     
+    @Override
+    public Film getFilm(String title) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Film> getFilms() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Actor> getActors(String filmTitle) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void create(Film film, List<Actor> actors) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Film f, String newTitle) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(String title) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
--- projet/src/main/java/com/mycompany/projet/services/FilmManagerLocal.java
+++ projet/src/main/java/com/mycompany/projet/services/FilmManagerLocal.java
@@ -5,6 +5,9 @@
  */
 package com.mycompany.projet.services;
 
+import com.mycompany.projet.models.Actor;
+import com.mycompany.projet.models.Film;
+import java.util.List;
 import javax.ejb.Local;
 
 /**
@@ -14,4 +17,10 @@ import javax.ejb.Local;
 @Local
 public interface FilmManagerLocal {
     
+    Film getFilm(String title);
+    List<Film> getFilms();
+    List<Actor> getActors(String filmTitle);
+    void create(Film film, List<Actor> actors);
+    void update(Film f, String newTitle);
+    void delete(String title);
 }
